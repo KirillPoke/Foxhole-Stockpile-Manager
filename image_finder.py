e99aba030b2fa94e9f8c25045277423d784b085e
Kirill <kr6512592@gmail.com>
2022-05-18 23:22:21 +0300

    Working POC

--- image_finder.py
+++ image_finder.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = 'F:\Coding\\tesseract.exe'
-#img = cv2.imread('C:\\Users\\Kirill\\Desktop\\stockpile.png')  # read image
 
 
 def get_number_contours(img):
     # Gel all pixels with the color of the stockpile number
-    gray = np.all(img == (98, 98, 98), 2)  # gray is a logical matrix with True where BGR = (34, 33, 33).
+    gray = np.all(img == (98, 98, 98), 2)
+    gray = gray.astype(np.uint8) * 255
+    # gray is a logical matrix with True where BGR = (34, 33, 33).
     # Find contours
     cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[
         -2]  # Use index [-2] to be compatible to OpenCV 3 and 4
@@ -18,17 +16,5 @@ def get_number_contours(img):
         if h > 20 and w > 30:  # Ignore gray pixels
             # Cropping the text block for giving input to OCR
             cropped = img[y:y + h, x:x + w]
-            filtered_cnts.append(cnts)
-            # Apply OCR on the cropped image
-            # text = pytesseract.image_to_string(cropped, config='--psm 8 outputbase digits')
-            # letters = string.ascii_lowercase
-            # file_name = ''.join(random.choice(letters) for i in range(10))
-            # if text:
-            #     texts.append(text)
-            #     recognized.append(cropped)
-            #     cv2.imwrite(f'C:\\Users\Kirill\\Desktop\\recognized\\{file_name}.png', cropped)
-            #     # cv2.imshow('crop', cropped)
-            #     # cv2.waitKey()
-            #     # cv2.destroyAllWindows()
-            # else:
-            #     cv2.imwrite(f'C:\\Users\Kirill\\Desktop\\recognized\\{file_name}.png', cropped)
+            filtered_cnts.append(cropped)
+    return filtered_cnts
--- main.py
+++ main.py
@@ -1,60 +1,14 @@
 import cv2
 import pytesseract
 
+from image_finder import get_number_contours
+
 pytesseract.pytesseract.tesseract_cmd = r'F:\\Coding\\tesseract.exe'
 import matplotlib.pyplot as plt
 import numpy as np
 from pytesseract import Output
 import os
 import pandas as pd
-from tqdm import tqdm
-
-
-def main(psm, digits, use_padding):
-    df_labels = pd.read_csv('labels_gold.csv').drop_duplicates()
-    labels_dict = dict(zip(df_labels['filename'], df_labels['label']))
-
-    dir = 'unrecognized'
-    margin = 4
-    if not digits:
-        custom_config = f'--oem 3 --psm {psm}'
-    else:
-        custom_config = f'--oem 3 --psm {psm} outputbase digits'
-
-    separator_digit = '39900993'
-    separator_img = cv2.imread('separator2.png')
-    separator_img = cv2.cvtColor(separator_img, cv2.COLOR_BGR2GRAY)
-    separator_img = cv2.bitwise_not(separator_img)
-    left_marg, right_marg, top_marg, bottom_marg = find_margins(separator_img)
-    separator_img = separator_img[top_marg - margin:bottom_marg + margin, left_marg - margin:right_marg + margin]
-
-    acc_list = []
-    for file in [f for f in os.listdir(dir) if f != '.DS_Store']:
-        true_label = labels_dict[file]
-        img = cv2.imread(f'{dir}/{file}')
-
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.bitwise_not(img)
-
-        left_marg, right_marg, top_marg, bottom_marg = find_margins(img)
-        img = img[top_marg - margin:bottom_marg + margin, left_marg - margin:right_marg + margin]
-
-        separator_img_resized = image_resize(separator_img, height=img.shape[0])
-
-        if use_padding:
-            # concatenate with separator image
-            img = np.concatenate([separator_img_resized, img, separator_img_resized], axis=1)
-
-        res = pytesseract.image_to_string(img, config=custom_config)
-
-        res = res.strip().replace('\n', '')
-
-        answer = res.replace(separator_digit, '').strip()
-
-        acc_list.append(int(answer == true_label))
-
-
-    return np.mean(acc_list)
 
 
 def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -83,11 +37,55 @@ def find_margins(img):
     return leftmost, rightmost, top, bottom
 
 
+margin = 4
+
+
+def add_padding(img):
+    separator_img = cv2.imread('separator2.png')
+    separator_img = cv2.cvtColor(separator_img, cv2.COLOR_BGR2GRAY)
+    separator_img = cv2.bitwise_not(separator_img)
+    left_marg, right_marg, top_marg, bottom_marg = find_margins(separator_img)
+
+    separator_img = separator_img[top_marg - margin:bottom_marg + margin, left_marg - margin:right_marg + margin]
+    separator_img_resized = image_resize(separator_img, height=img.shape[0])
+    padded_img = np.concatenate([separator_img_resized, img, separator_img_resized], axis=1)
+    return padded_img
+
+
+def pre_process(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.bitwise_not(img)
+
+    left_marg, right_marg, top_marg, bottom_marg = find_margins(img)
+    img = img[top_marg - margin:bottom_marg + margin, left_marg - margin:right_marg + margin]
+    return img
+
+
+def validate_padding_found(string, padding_string):
+    if string.count(padding_string) != 2:
+        print("Did not found correct padding")
+        #raise AssertionError("Did not found correct padding")
+
+def show_img(img):
+    cv2.imshow('graycsale image', img)
+    cv2.waitKey(0)
+
+def fancy_img_to_string(img):
+    separator_digit = '39900993'
+    custom_config = f'--oem 3 --psm 7'
+    pre_processed_img = pre_process(img)
+    padded_img = add_padding(pre_processed_img)
+    string_from_image = pytesseract.image_to_string(padded_img, config=custom_config)
+    validate_padding_found(string_from_image, separator_digit)
+    string_from_image = string_from_image.strip().replace('\n', '')
+    string_from_image = string_from_image.replace(separator_digit, '').strip()
+    return string_from_image
+
+
 if __name__ == '__main__':
-    res_dict = {'psm': [], 'accuracy': []}
-    psm = 7
-    acc = main(psm, False, True)
-    res_dict['psm'].append(psm)
-    res_dict['accuracy'].append(acc)
-    print(acc)
-    pd.DataFrame(res_dict).to_csv('grid_search_res.csv', index=False)
+    dir = 'unrecognized'
+    img = cv2.imread('C:\\Users\\Kirill\\Desktop\\stockpile.png')  # read image
+    contours = get_number_contours(img)
+    for cnt in contours:
+        string_from_image = fancy_img_to_string(cnt)
+        print(string_from_image)
